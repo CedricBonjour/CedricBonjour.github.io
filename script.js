1a42547d1a1449eeb9b0c15d16d335c42d8ae138
Cedric Bonjour <cbonjour37@gmail.com>
2020-08-25 19:15:46 +0200

    JHU data sci spe updated

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 
-
-function startup(){
-  document.querySelectorAll('.detailed').forEach(item => {
-    item.addEventListener('click', event => {
-      
-      if(item.style.backgroundImage !='none'){
-        item.style.backgroundImage ='none';
-      }else{
-        item.style.backgroundImage ='url(nav/down.svg)';
-      }
-      item.getElementsByTagName("description")[0].classList.toggle('show');
-    })
+Array.from(document.getElementsByClassName("detailed")).forEach(item => {
+  item.addEventListener('click', event => {
+    if(item.style.backgroundImage !='none')item.style.backgroundImage ='none';
+    else item.style.backgroundImage ='url(nav/down.svg)';
+    item.getElementsByTagName("description")[0].classList.toggle('show');
   })
-}
+})
+
