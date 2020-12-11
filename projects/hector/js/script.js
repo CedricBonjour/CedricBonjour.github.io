dd1223336601b1b81a1f6404e36bef6bea849379
Cedric Bonjour <cbonjour37@gmail.com>
2020-12-11 14:17:15 +0100

    hector no clip path version

--- projects/hector/js/script.js
+++ projects/hector/js/script.js
@@ -1,117 +1,48 @@
 window.addEventListener("load", e=>{
 
 
-
+//================================================================UI & LAYOUT
 const bg = document.getElementById("bg");
-const consultants = document.getElementsByClassName("consultant")
 const cover_screen = document.getElementById("cover_screen");
 const img_container = document.getElementById("img_container");
 const wrapper = document.getElementById("wrapper");
-
 const small_img = document.getElementById("small_img");
 const header = document.getElementById("header");
 const txt = document.getElementById("txt");
 
-const start_game = document.getElementById("start_game");
-const go_to_game = document.getElementById("go_to_game");
-const keep_looking = document.getElementById("keep_looking");
-const incriminate = document.getElementById("incriminate");
+
 
 const ratio  =  bg.width / bg.height;
-const win_count = consultants.length; 
+//const win_count = consultants.length; 
 var found_count = 0;
 
 var size_coef = 1.001;
 
 
 
-//================================OBSERVER
-
-// background.addEventListener('transitionend', function(e){
-
-//         img_container.style.width = background.width+"px";
-//     img_container.style.height = background.height+"px";
-// });
-            
-
-
-// var observer = new MutationObserver(function(mutations) { 
-//     img_container.style.width = background.width+"px";
-//     img_container.style.height = background.height+"px";
-// });
-
-// observer.observe(background, { attributes : true, attributeFilter : ['width'] });
-
-
-//==================================STEPS
-
-
-
-function runResize(e,b = "smooth"){
-    const new_width = size_coef* Math.max(window.innerWidth, window.innerHeight *ratio) ;
-    img_container.style.width = new_width+"px";
-    img_container.style.height = (new_width /ratio )+"px";
-    var st =(wrapper.scrollHeight - wrapper.clientHeight) / 2;
-    var sl= (wrapper.scrollWidth - wrapper.clientWidth) / 2;
-    wrapper.scroll({top: st,left: sl,behavior:b }); //smooth
-}
-
-
-
-// window.addEventListener('resize', runResize);
-runResize(null,"auto")
-
+resize_img()
 img_container.style.transition = "width 1s ease-in-out,height 1s ease-in-out"
 
-for (consultant of consultants)consultant.addEventListener("click", found_consultant);
+function resize_img (coef = 1 , b="auto"){
+    let screen_ratio  = window.innerWidth/ window.innerHeight
+    if (screen_ratio > ratio){
+        console.log("Case 1")
+        img_container.style.width = (100*coef)+"vw";
+        img_container.style.height = (100*coef /ratio )+"vw";
 
-go_to_game.addEventListener("click", e=>{
-    header.innerHTML = "Who  [...]  amongst Hectors?"
-    txt.innerHTML = "Ask around to figure out!";
-    // small_img.src = "pika2.png"
-    start_game.style.display = "block";
-    go_to_game.style.display = "none";   
-})
+    }else{       
+        console.log("Case 2")
+        img_container.style.height = (100*coef)+"vh";
+        img_container.style.width = (100*coef *ratio )+"vh";
 
-start_game.addEventListener("click", e=>{
-    start_game.style.display = "none";
-    keep_looking.style.display = "inline-block";
-    incriminate.style.display = "inline-block";
-    cover_screen.style.display = "none";
-    size_coef = 1.3;
-    runResize(null, "smooth");
-    
-})
+    }
+    var st =(wrapper.scrollHeight - wrapper.clientHeight) / 2;
+    var sl= (wrapper.scrollWidth - wrapper.clientWidth) / 2;
+    wrapper.scroll({top: st,left: sl,behavior:b });
 
-
-keep_looking.addEventListener("click", e=>{
-    keep_looking.style.display = "inline-block";
-    incriminate.style.display = "inline-block";
-    cover_screen.style.display = "none"; 
-})
-
-
-function found_consultant (e){
-    e.target.style.opacity = 0.6; 
-    cover_screen.style.display = "flex";
-    small_img.src = "profile_img/" + e.target.id + ".png"
-    header.innerHTML = capitalizeFirstLetter( e.target.id);
-    var img = document.createElement('img'); 
-    img.src = "./check.svg"
-    txt.innerHTML = dialogue[e.target.id];
-    console.log(found_count);
-
-    if(found_count >= win_count) win();
-   
 }
-
 
 function capitalizeFirstLetter(string) {return string.charAt(0).toUpperCase() + string.slice(1)}
-
-function win(){
-    win_screen.style.display = "flex"
-
-}
 
 
 
@@ -152,47 +83,84 @@ wrapper.addEventListener("mousemove", e => {
 });
 
 
-// 
-//     img_container.style.cursor = 'grab';
+//================================ CONSULTANTS
 
-//     let pos = { top: 0, left: 0, x: 0, y: 0 };
+const consultants = document.getElementsByClassName("consultant")
+for (consultant of consultants)consultant.addEventListener("click", found_consultant);
 
-//     const mouseDownHandler = function(e) {
-//         img_container.style.cursor = 'grabbing';
-//         img_container.style.userSelect = 'none';
+//==============================BUTTON LOGIC
 
-//         pos = {
-//             left: img_container.scrollLeft,
-//             top: img_container.scrollTop,
-//             // Get the current mouse position
-//             x: e.clientX,
-//             y: e.clientY,
-//         };
 
-//         document.addEventListener('mousemove', mouseMoveHandler);
-//         document.addEventListener('mouseup', mouseUpHandler);
-//     };
 
-//     const mouseMoveHandler = function(e) {
-//         // How far the mouse has been moved
-//         const dx = e.clientX - pos.x;
-//         const dy = e.clientY - pos.y;
 
-//         // Scroll the element
-//         img_container.scrollTop = pos.top - dy;
-//         img_container.scrollLeft = pos.left - dx;
-//     };
 
-//     const mouseUpHandler = function() {
-//         img_container.style.cursor = 'grab';
-//         img_container.style.removeProperty('user-select');
+const go_to_game = document.getElementById("go_to_game");
+const start_game = document.getElementById("start_game");
+const keep_looking = document.getElementById("keep_looking");
+const incriminate = document.getElementById("incriminate");
+const try_again = document.getElementById("try_again");
+const play_again = document.getElementById("play_again");
 
-//         document.removeEventListener('mousemove', mouseMoveHandler);
-//         document.removeEventListener('mouseup', mouseUpHandler);
-//     };
+go_to_game.style.display = "block";
 
-//     // Attach the handler
-//     img_container.addEventListener('mousedown', mouseDownHandler);
-// 
+
+
+
+function found_consultant (e){
+    e.target.style.opacity = 1; 
+    cover_screen.style.display = "flex";
+    small_img.src = "profile_img/" + e.target.id + ".png"
+    header.innerHTML = capitalizeFirstLetter( e.target.id);
+    txt.innerHTML = dialogue[e.target.id];  
+    keep_looking.style.display = "inline-block";
+    incriminate.style.display = "inline-block"; 
+}
+
+
+go_to_game.addEventListener("click", e=>{
+    header.innerHTML = "Who  [...]  amongst Hectors?"
+    txt.innerHTML = "Ask around to figure out!";
+    start_game.style.display = "block";
+    go_to_game.style.display = "none";   
+})
+
+start_game.addEventListener("click", e=>{
+    start_game.style.display = "none";
+    keep_looking.style.display = "inline-block";
+    incriminate.style.display = "inline-block";
+    cover_screen.style.display = "none";
+   
+    resize_img(1.3, "smooth");
+    
+})
+
+
+keep_looking.addEventListener("click", e=>{
+
+    cover_screen.style.display = "none"; 
+})
+
+
+incriminate.addEventListener("click", e=>{
+    incriminate.style.display = "none";
+    if (header.innerHTML == "Hector" ){
+        keep_looking.style.display = "none";
+        play_again.style.display = "block";
+        header.innerHTML = "That's right. You WIN!"
+        txt.innerHTML  = "Hector's idea was indeed the best" 
+    }else{
+        header.innerHTML = "A great idea indeed, but we've got better!"
+        txt.innerHTML  = "You should probably keep looking" 
+    }
+   
+   
+})
+
+play_again.addEventListener("click", e=>{
+
+  location.reload();
+})
+
+
 
 });
--- projects/hector/js/text.js
+++ projects/hector/js/text.js
@@ -1,3 +1,6 @@
+const lost_text = "A great idea indeed, but we've got better!"
+
+
 const dialogue ={
     pikachu:"Please help me find my muffins!",
     dracofeu:"I'm probably as big as the culprit but I have honor. <h3>I would never do such a thing.</h3>",
